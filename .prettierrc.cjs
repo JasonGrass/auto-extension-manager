ecf2e245a7f4cc0c7eddd911924096d5e01db200
JasonGrass <junjiegrass@qq.com>
2023-05-31 15:27:06 +0800

    modify prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  importOrder: ["^~(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+}
