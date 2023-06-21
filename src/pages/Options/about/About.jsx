bc4466acb278c2548ee08307421d7545a89b1150
JasonGrass <junjiegrass@qq.com>
2023-06-21 11:57:36 +0800

    添加 About 页面 UI

--- src/pages/Options/about/About.jsx
+++ src/pages/Options/about/About.jsx
@@ -1,12 +1,61 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
+import { Button } from "antd"
+import newGithubIssueUrl from "new-github-issue-url"
+
+import Icon from ".../assets/img/icon-128.png"
 import Title from "../Title.jsx"
 import { AboutStyle } from "./AboutStyle"
 
 function About() {
+  const [version, setVersion] = useState("UNKNOWN")
+
+  useEffect(() => {
+    chrome.management.getSelf((self) => {
+      setVersion(self.version)
+    })
+  }, [])
+
+  const openIssue = () => {
+    const url = newGithubIssueUrl({
+      user: "JasonGrass",
+      repo: "auto-extension-manager",
+      body: `
+\n\n\n---
+<!-- ↑请在此行上方填写问题/建议详情↑ -->
+Extension Manager ${version}
+${navigator.userAgent}`
+    })
+
+    chrome.tabs.create({ url })
+  }
+
   return (
     <AboutStyle>
       <Title title="关于"></Title>
+
+      <div className="header-icon">
+        <img src={Icon} alt="icon" />
+        <div className="header-icon-text">
+          <h3>Extension Manager</h3>
+          <span>一个可以根据规则自动打开或关闭浏览器扩展的工具</span>
+        </div>
+      </div>
+
+      <div className="content-button">
+        <Button onClick={openIssue}>反馈问题</Button>
+        <Button>查看帮助</Button>
+      </div>
+
+      <div className="footer">
+        <span>版本 {version}</span>
+        <a
+          href="https://github.com/JasonGrass/auto-extension-manager"
+          target="_blank"
+          rel="noreferrer">
+          Github
+        </a>
+      </div>
     </AboutStyle>
   )
 }
--- src/pages/Options/about/AboutStyle.js
+++ src/pages/Options/about/AboutStyle.js
@@ -1,3 +1,56 @@
 import styled from "styled-components"
 
-export const AboutStyle = styled.div``
+export const AboutStyle = styled.div`
+  .header-icon {
+    display: flex;
+    align-items: center;
+
+    margin-top: 30px;
+    margin-bottom: 50px;
+
+    img {
+      width: 64px;
+      height: 64px;
+    }
+
+    .header-icon-text {
+      margin-left: 8px;
+      h3 {
+        font-size: 18px;
+        margin-bottom: 4px;
+      }
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+
+  .content-button {
+    & > * {
+      margin-right: 10px;
+    }
+  }
+
+  .footer {
+    display: flex;
+    flex-direction: column;
+
+    margin-top: 50px;
+
+    span {
+      font-size: 14px;
+      margin-bottom: 10px;
+    }
+
+    a {
+      color: #337ab7;
+      font-size: 14px;
+      text-decoration: none;
+
+      &:hover {
+        color: #23527c;
+        text-decoration: underline;
+      }
+    }
+  }
+`
