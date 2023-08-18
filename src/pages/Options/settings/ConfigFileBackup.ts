1c7a8af62cc4b51d3317fbc0c3996932d059b38e
JasonGrass <junjiegrass@qq.com>
2023-08-18 18:20:03 +0800

    调整导入配置 json 文件的命名

--- src/pages/Options/settings/ConfigFileBackup.ts
+++ src/pages/Options/settings/ConfigFileBackup.ts
@@ -13,7 +13,7 @@ export async function exportConfig() {
     ruleConfig: config.ruleConfig,
     management: config.management
   }
-  exportToJsonFile(data, `ext_manager_config_${new Date().toLocaleString()}.json`)
+  exportToJsonFile(data, `ext_manager_config_${formatDate(new Date())}.json`)
 }
 
 /**
@@ -136,4 +136,15 @@ function exportToJsonFile(data: any, filename: string) {
 
   document.body.removeChild(downloadLink)
   URL.revokeObjectURL(downloadLink.href)
+}
+
+function formatDate(date: Date) {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
+  const hours = String(date.getHours()).padStart(2, "0")
+  const minutes = String(date.getMinutes()).padStart(2, "0")
+  const seconds = String(date.getSeconds()).padStart(2, "0")
+
+  return `${year}${month}${day}_${hours}${minutes}${seconds}`
 }
