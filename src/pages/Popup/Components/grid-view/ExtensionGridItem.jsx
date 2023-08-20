4eefa8a93161d6eaba842d1da414720006c6f069
JasonGrass <junjiegrass@qq.com>
2023-08-20 12:47:39 +0800

    添加 grid 布局显示

--- src/pages/Popup/Components/grid-view/ExtensionGridItem.jsx
+++ src/pages/Popup/Components/grid-view/ExtensionGridItem.jsx
@@ -1,7 +1,30 @@
 import React, { memo } from "react"
 
-const ExtensionGridItem = memo(() => {
-  return <div>ExtensionGridItem</div>
+import { styled } from "styled-components"
+
+import { getIcon } from ".../utils/extensionHelper.js"
+
+const ExtensionGridItem = memo(({ item, options }) => {
+  return (
+    <Style>
+      <div>
+        <img src={getIcon(item, 48)} alt="icon" />
+      </div>
+    </Style>
+  )
 })
 
 export default ExtensionGridItem
+
+const Style = styled.div`
+  transition: transform 0.3s ease;
+
+  img {
+    width: 42px;
+    height: 42px;
+  }
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`
--- src/pages/Popup/Components/grid-view/ExtensionGridView.jsx
+++ src/pages/Popup/Components/grid-view/ExtensionGridView.jsx
@@ -1,7 +1,89 @@
-import React, { memo } from "react"
+import React, { memo, useEffect, useState } from "react"
+
+import classNames from "classnames"
+import { styled } from "styled-components"
+
+import { appendAdditionInfo, sortExtension } from ".../utils/extensionHelper"
+import ExtensionGridItem from "./ExtensionGridItem"
 
 const ExtensionGrid = memo(({ extensions, options }) => {
-  return <div>ExtensionGridView</div>
+  const [items1, setItems1] = useState([])
+  const [items2, setItems2] = useState([])
+
+  useEffect(() => {
+    const list = appendAdditionInfo(extensions, options)
+    const list1 = list.filter((i) => i.enabled)
+    const list2 = list.filter((i) => !i.enabled)
+    setItems1(sortExtension(list1))
+    setItems2(sortExtension(list2))
+  }, [extensions, options])
+
+  return (
+    <Style>
+      <ul>
+        {items1.map((item) => {
+          return (
+            <li key={item.id} className="grid-item-enable">
+              <ExtensionGridItem item={item} options={options}></ExtensionGridItem>
+            </li>
+          )
+        })}
+        {new Array(10).fill("").map(() => (
+          <i></i>
+        ))}
+      </ul>
+      {items2.length > 0 && <div className="divider"></div>}
+      <ul>
+        {items2.map((item) => {
+          return (
+            <li key={item.id} className="grid-item-disable">
+              <ExtensionGridItem item={item} options={options}></ExtensionGridItem>
+            </li>
+          )
+        })}
+        {new Array(10).fill("").map(() => (
+          <i></i>
+        ))}
+      </ul>
+      <div className="divider"></div>
+    </Style>
+  )
 })
 
 export default ExtensionGrid
+const Style = styled.div`
+  margin-top: 10px;
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+    align-content: flex-start;
+    flex-wrap: wrap;
+
+    li {
+      width: 42px;
+      height: 42px;
+
+      margin: 12px 10px;
+    }
+
+    i {
+      width: 42px;
+      margin: 0px 10px;
+    }
+  }
+
+  .divider {
+    height: 1px;
+    background-color: #ccc;
+    margin: 0px 10px 10px 10px;
+  }
+
+  .grid-item-disable {
+    filter: grayscale(100%) opacity(50%);
+
+    &:hover {
+      filter: none;
+    }
+  }
+`
