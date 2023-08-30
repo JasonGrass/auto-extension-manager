6395e1af39589da5b6cc5ea3d1156e8680431b8b
JasonGrass <junjiegrass@qq.com>
2023-08-30 19:38:18 +0800

    添加历史记录 table 的 css 样式

--- src/pages/Options/history/ExtensionHistoryStyle.js
+++ src/pages/Options/history/ExtensionHistoryStyle.js
@@ -1,5 +1,28 @@
 import { styled } from "styled-components"
 
-const Style = styled.div``
+const Style = styled.div`
+  .ant-table-wrapper {
+    margin-right: 5px;
+  }
+
+  .column-index {
+    display: inline-block;
+    width: 100%;
+    text-align: center;
+  }
+
+  .column-name {
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: 5px;
+    }
+  }
+
+  .ant-table-expanded-row .ant-table-cell {
+    padding-bottom: 2px;
+  }
+`
 
 export default Style
