4dc1a5a63d6bd2eb95465f957cba0bff77006f2e
JasonGrass <junjiegrass@qq.com>
2023-08-31 21:41:26 +0800

    添加历史记录的最大数量限制

--- src/pages/Background/history/HistoryRepo.ts
+++ src/pages/Background/history/HistoryRepo.ts
@@ -2,6 +2,9 @@ import Dexie from "dexie"
 
 import { HistoryRecord } from "./Record"
 
+const MAX_HISTORY_COUNT = 1100
+const DELETE_COUNT_ONCE = 100
+
 export class HistoryRepo extends Dexie {
   private history!: Dexie.Table<Partial<HistoryRecord>, number>
 
@@ -15,10 +18,17 @@ export class HistoryRepo extends Dexie {
   public async add(record: Partial<HistoryRecord>) {
     record.id = undefined // 自增 ID，这里设置成 undefined，数据库自行赋值
     await this.history.add(record)
+
+    const count = await this.history.count()
+    if (count > MAX_HISTORY_COUNT) {
+      const oldRecords = await this.history.orderBy("timestamp").limit(DELETE_COUNT_ONCE).toArray()
+      const keys = oldRecords.map((r) => r.id) as number[]
+      await this.history.bulkDelete(keys)
+    }
   }
 
   public async getAll(): Promise<Partial<HistoryRecord>[]> {
-    return await this.history.toArray()
+    return await this.history.limit(MAX_HISTORY_COUNT - DELETE_COUNT_ONCE).toArray()
   }
 
   public async clearAll() {
