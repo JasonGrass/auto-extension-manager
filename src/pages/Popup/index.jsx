9c93347461075ef2b4cc2b54b198fec484fd4850
JasonGrass <junjiegrass@qq.com>
2023-10-02 12:53:28 +0800

    refactor 将 popup prepare 的部分抽离，使用缓存来优化配置读取速度

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -8,54 +8,16 @@ import "antd/dist/reset.css"
 import { message } from "antd"
 import { ConfigProvider, theme } from "antd"
 import { ThemeProvider } from "styled-components"
-import chromeP from "webext-polyfill-kinda"
 
 import "./index.css"
 
-import { getPopupWidth } from ".../pages/Popup/utils/popupLayoutHelper"
 import storage from ".../storage/sync"
-import { appendAdditionInfo } from ".../utils/extensionHelper"
 import { ExtensionIconBuilder } from "../Background/extension/ExtensionIconBuilder"
 import Popup from "./Components/Popup"
+import { prepare } from "./prepare"
 
 const container = document.getElementById("app-container")
 const root = createRoot(container)
-
-const prepare = async function () {
-  let allExtensions = await chromeP.management.getAll()
-  const allOptions = await storage.options.getAll()
-
-  // 如果关闭了在 Popup 中显示固定分组中的扩展，则隐藏这些扩展
-  if (!(allOptions.setting.isShowFixedExtension ?? true)) {
-    const fixedGroup = allOptions.groups.find((g) => g.id === "fixed")
-    if (fixedGroup && fixedGroup.extensions) {
-      allExtensions = allExtensions.filter((ext) => !fixedGroup.extensions.includes(ext.id))
-    }
-  }
-  // 不展示主题类的扩展
-  allExtensions = allExtensions.filter((ext) => ext.type !== "theme")
-  // 不展示自己
-  const self = await chromeP.management.getSelf()
-  allExtensions = allExtensions.filter((ext) => ext.id !== self.id)
-
-  const extensions = appendAdditionInfo(allExtensions, allOptions.management)
-
-  // popup 宽度设置
-  document.body.style.width = getPopupWidth(
-    allOptions.setting.layout,
-    allExtensions.count,
-    allOptions.setting.columnCountInGirdView
-  )
-
-  return {
-    // 插件信息
-    extensions: extensions,
-    // 用户配置信息
-    options: allOptions,
-    // 运行时临时参数
-    params: {}
-  }
-}
 
 const storageViewApi = storage.helper.view.getApi()
 storageViewApi.message = message
---
+++ src/pages/Popup/prepare.js
@@ -0,0 +1,94 @@
+import localforage from "localforage"
+
+import { getPopupWidth } from ".../pages/Popup/utils/popupLayoutHelper"
+import storage from ".../storage/sync"
+import { appendAdditionInfo } from ".../utils/extensionHelper"
+
+const forage = localforage.createInstance({
+  driver: localforage.LOCALSTORAGE,
+  name: "TempCache",
+  version: 1.0,
+  storeName: "options"
+})
+
+export const prepare = async function () {
+  const allOptions = await getOptions()
+
+  const extensions = await getShownExtensions(allOptions)
+
+  // popup 宽度设置
+  document.body.style.width = getPopupWidth(
+    allOptions.setting.layout,
+    extensions.count,
+    allOptions.setting.columnCountInGirdView
+  )
+
+  return {
+    // 插件信息
+    extensions: extensions,
+    // 用户配置信息
+    options: allOptions,
+    // 运行时临时参数
+    params: {}
+  }
+}
+
+/**
+ * 获取要显示的扩展
+ */
+async function getShownExtensions(allOptions) {
+  let allExtensions = await chrome.management.getAll()
+
+  // 如果关闭了在 Popup 中显示固定分组中的扩展，则隐藏这些扩展
+  if (!(allOptions.setting.isShowFixedExtension ?? true)) {
+    const fixedGroup = allOptions.groups.find((g) => g.id === "fixed")
+    if (fixedGroup && fixedGroup.extensions) {
+      allExtensions = allExtensions.filter((ext) => !fixedGroup.extensions.includes(ext.id))
+    }
+  }
+
+  // 不展示主题类的扩展，不展示自己
+  const selfId = await getSelfId()
+  allExtensions = allExtensions
+    .filter((ext) => ext.type !== "theme")
+    .filter((ext) => ext.id !== selfId)
+
+  // 填充附加信息
+  const extensions = appendAdditionInfo(allExtensions, allOptions.management)
+
+  return extensions
+}
+
+/**
+ * 获取所有配置信息，优先从缓存中读取
+ */
+async function getOptions() {
+  const cacheOptions = await forage.getItem("all_options")
+  if (!cacheOptions) {
+    const allOptions = await storage.options.getAll()
+    await forage.setItem("all_options", allOptions)
+    return allOptions
+  } else {
+    // 更新缓存的配置信息
+    setTimeout(async () => {
+      const allOptions = await storage.options.getAll()
+      await forage.setItem("all_options", allOptions)
+      console.log("update cache all_options")
+    }, 3000)
+    return cacheOptions
+  }
+}
+
+/**
+ * 获取自身的扩展ID，优先从缓存中读取
+ */
+async function getSelfId() {
+  const selfId = await forage.getItem("self_id")
+  if (selfId) {
+    return selfId
+  }
+
+  const self = await chrome.management.getSelf()
+  await forage.setItem("self_id", self.id)
+  return self.id
+}
