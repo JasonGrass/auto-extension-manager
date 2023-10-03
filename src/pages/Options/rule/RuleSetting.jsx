f11947c3fb7cb82467723622c0d87f3e17d2427d
JasonGrass <junjiegrass@qq.com>
2023-10-03 11:10:45 +0800

    add 添加规则处理的埋点

--- src/pages/Options/rule/RuleSetting.jsx
+++ src/pages/Options/rule/RuleSetting.jsx
@@ -4,6 +4,7 @@ import chromeP from "webext-polyfill-kinda"
 
 import storage from ".../storage/sync"
 import { appendAdditionInfo, filterExtensions, isExtExtension } from ".../utils/extensionHelper.js"
+import analytics from ".../utils/googleAnalyze.js"
 import { getLang } from ".../utils/utils"
 import Title from "../Title.jsx"
 import { RuleSettingStyle } from "./RuleSettingStyle.js"
@@ -13,7 +14,7 @@ function RuleSetting() {
   const [extensions, setExtensions] = useState([])
 
   // 所有的规则配置项，列表
-  const [ruleConfigs, setRuleConfigs] = useState(null)
+  const [ruleConfigs, setRuleConfigs] = useState([])
 
   // 用户配置
   const [options, setOptions] = useState({})
@@ -47,18 +48,34 @@ function RuleSetting() {
     delete: async (id) => {
       await storage.rule.deleteOne(id)
       updateRuleConfig()
+
+      analytics.fireEvent("rule-delete", {
+        total_count: ruleConfigs.length - 1
+      })
     },
     add: async (record) => {
       await storage.rule.addOne(record)
       updateRuleConfig()
+
+      analytics.fireEvent("rule-add", {
+        total_count: ruleConfigs.length + 1
+      })
     },
     update: async (record) => {
       await storage.rule.update(record)
       updateRuleConfig()
+
+      analytics.fireEvent("rule-update", {
+        total_count: ruleConfigs.length
+      })
     },
     duplicate: async (record) => {
       await storage.rule.duplicate(record)
       updateRuleConfig()
+
+      analytics.fireEvent("rule-duplicate", {
+        total_count: ruleConfigs.length + 1
+      })
     }
   }
 
