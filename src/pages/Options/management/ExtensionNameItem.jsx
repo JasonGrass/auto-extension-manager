624e695055bc0a60ac88bb25b35f55c7c5eaa48b
JasonGrass <junjiegrass@qq.com>
2023-10-11 19:25:56 +0800

    🐞 fix(management): 删除名称禁用样式

--- src/pages/Options/management/ExtensionNameItem.jsx
+++ src/pages/Options/management/ExtensionNameItem.jsx
@@ -10,25 +10,11 @@ import ExtensionChannelLabel from "./ExtensionChannelLabel"
  * 扩展管理设置中的列表 Item 项
  */
 const ExtensionNameItem = memo(({ name, record }) => {
-  const [itemEnable, setItemEnable] = useState(record.enabled)
-
-  useEffect(() => {
-    setItemEnable(record.enabled)
-  }, [record])
-
   return (
     <Tooltip placement="topLeft" title={name}>
       <Style>
         <img src={record.icon} alt="" width={16} height={16} />
-        <span
-          className={classNames([
-            "column-name-title",
-            {
-              "column-name-title-disable": !itemEnable
-            }
-          ])}>
-          {name}
-        </span>
+        <span className={classNames(["column-name-title"])}>{name}</span>
         <ExtensionChannelLabel channel={record.channel}></ExtensionChannelLabel>
       </Style>
     </Tooltip>
@@ -49,9 +35,5 @@ const Style = styled.span`
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
-  }
-
-  .column-name-title-disable {
-    color: #c1c1c1;
   }
 `
