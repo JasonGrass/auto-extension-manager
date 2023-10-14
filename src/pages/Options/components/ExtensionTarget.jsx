fd554e9432425c3cb583ff55700658bd4fa38279
JasonGrass <junjiegrass@qq.com>
2023-10-14 10:15:21 +0800

    🦄 refactor(rule): 将 ExtensionTarget 的 Header 抽离出去

--- src/pages/Options/components/ExtensionTarget.jsx
+++ src/pages/Options/components/ExtensionTarget.jsx
@@ -11,7 +11,7 @@ import ExtensionItems from "./ExtensionItems"
 
 const { CheckableTag } = Tag
 
-const ExtensionTarget = ({ options, config, extensions, params }, ref) => {
+const ExtensionTarget = ({ options, config, extensions, searchText, params, children }, ref) => {
   const groupList = storage.helper.formatGroups(options.groups)
 
   let emptyMessage = params.emptyMessage
@@ -42,9 +42,6 @@ const ExtensionTarget = ({ options, config, extensions, params }, ref) => {
   const [unselectedExtensions, setUnselectedExtensions] = useState([])
   // 显示到界面上的剩余扩展（搜索之后的结果）
   const [displayUnselectedExtensions, setDisplayUnselectedExtensions] = useState([])
-
-  // 搜索关键字
-  const [searchText, setSearchText] = useState("")
 
   // 根据配置进行初始化
   useEffect(() => {
@@ -104,12 +101,6 @@ const ExtensionTarget = ({ options, config, extensions, params }, ref) => {
     setSelectedExtensions(selected)
   }
 
-  // 搜索扩展
-  const onSearchTextChange = (e) => {
-    const text = e.target.value
-    setSearchText(text)
-  }
-
   // 选择的扩展组方法变化
   const handleSelectGroupChange = (groupId, checked) => {
     const nextSelectedGroupIds = checked
@@ -121,17 +112,7 @@ const ExtensionTarget = ({ options, config, extensions, params }, ref) => {
   return (
     <EditorCommonStyle>
       <Style>
-        <div className="editor-step-header">
-          <span className="title">2 {getLang("rule_set_target_title")}</span>
-
-          <SearchStyle>
-            <input
-              type="text"
-              placeholder="Search"
-              value={searchText}
-              onChange={(e) => onSearchTextChange(e)}></input>
-          </SearchStyle>
-        </div>
+        {children}
 
         <div className="group-match-mode-container">
           <span className="select-group-label">
@@ -196,29 +177,6 @@ const Style = styled.div`
     & h3 {
       font-weight: bold;
       margin: 20px 0;
-    }
-  }
-`
-
-const SearchStyle = styled.div`
-  position: absolute;
-  top: -2px;
-  left: 240px;
-
-  padding-left: 36px;
-
-  input {
-    width: 200px;
-    height: 24px;
-
-    outline-style: none;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-
-    &:focus {
-      border-color: #66afe9;
-      outline: 0;
-      box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 4px rgba(102, 175, 233, 0.6);
     }
   }
 `
--- src/pages/Options/rule/editor/ExtensionSelector.jsx
+++ src/pages/Options/rule/editor/ExtensionSelector.jsx
@@ -1,12 +1,24 @@
-import React, { forwardRef, memo, useImperativeHandle, useRef } from "react"
+import React, { forwardRef, memo, useImperativeHandle, useRef, useState } from "react"
+
+import styled from "styled-components"
 
 import ExtensionTarget from ".../pages/Options/components/ExtensionTarget"
 import { getLang } from ".../utils/utils"
 
 const ExtensionSelector = ({ options, config, extensions }, ref) => {
   const emptyMessage = getLang("rule_set_target_no_any_target")
+  const title = `2 ${getLang("rule_set_target_title")}`
 
   const selectorRef = useRef(null)
+
+  // 搜索关键字
+  const [searchText, setSearchText] = useState("")
+
+  // 搜索扩展
+  const onSearchTextChange = (e) => {
+    const text = e.target.value
+    setSearchText(text)
+  }
 
   useImperativeHandle(ref, () => ({
     // 获取配置
@@ -20,9 +32,44 @@ const ExtensionSelector = ({ options, config, extensions }, ref) => {
       options={options}
       config={config}
       extensions={extensions}
+      searchText={searchText}
       params={{ emptyMessage }}
-      ref={selectorRef}></ExtensionTarget>
+      ref={selectorRef}>
+      <div className="editor-step-header">
+        <span className="title">{title}</span>
+        <SearchStyle>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchText}
+            onChange={(e) => onSearchTextChange(e)}></input>
+        </SearchStyle>
+      </div>
+    </ExtensionTarget>
   )
 }
 
 export default memo(forwardRef(ExtensionSelector))
+
+const SearchStyle = styled.div`
+  position: absolute;
+  top: -2px;
+  left: 240px;
+
+  padding-left: 36px;
+
+  input {
+    width: 200px;
+    height: 24px;
+
+    outline-style: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+
+    &:focus {
+      border-color: #66afe9;
+      outline: 0;
+      box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 4px rgba(102, 175, 233, 0.6);
+    }
+  }
+`
