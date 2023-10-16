b5258095c773485fa08507864d2c70279f0d1208
JasonGrass <junjiegrass@qq.com>
2023-10-16 11:18:24 +0800

    🦄 refactor(utils): 抽取 formatDate

--- src/pages/Options/settings/ConfigFileBackup.ts
+++ src/pages/Options/settings/ConfigFileBackup.ts
@@ -1,5 +1,5 @@
 import storage from ".../storage/sync"
-import { downloadFile } from ".../utils/utils"
+import { downloadFile, formatDate } from ".../utils/utils"
 import ConvertRuleToV2 from "../../Background/rule/RuleConverter"
 
 /**
@@ -129,15 +129,4 @@ function exportToJsonFile(data: any, filename: string) {
   const blob = new Blob([jsonStr], { type: "application/json" })
 
   downloadFile(blob, filename)
-}
-
-function formatDate(date: Date) {
-  const year = date.getFullYear()
-  const month = String(date.getMonth() + 1).padStart(2, "0")
-  const day = String(date.getDate()).padStart(2, "0")
-  const hours = String(date.getHours()).padStart(2, "0")
-  const minutes = String(date.getMinutes()).padStart(2, "0")
-  const seconds = String(date.getSeconds()).padStart(2, "0")
-
-  return `${year}${month}${day}_${hours}${minutes}${seconds}`
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -73,3 +73,17 @@ export const downloadFile = (contentBlob, filename) => {
   document.body.removeChild(downloadLink)
   URL.revokeObjectURL(downloadLink.href)
 }
+
+/**
+ * 对时间进行格式化输出，输出为 yyMMDD_HHmmss
+ */
+export function formatDate(date) {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
+  const hours = String(date.getHours()).padStart(2, "0")
+  const minutes = String(date.getMinutes()).padStart(2, "0")
+  const seconds = String(date.getSeconds()).padStart(2, "0")
+
+  return `${year}${month}${day}_${hours}${minutes}${seconds}`
+}
