da9c5e1e78a64c61f44d04e231616484644557bc
JasonGrass <junjiegrass@qq.com>
2023-10-18 10:14:20 +0800

    🐞 fix(置顶显示): 修复置顶显示的排序

--- src/pages/Popup/utils/usePopupExtensions.js
+++ src/pages/Popup/utils/usePopupExtensions.js
@@ -36,14 +36,10 @@ export const usePopupExtensions = (extensions, options, moved) => {
     if (list0.length === 0 && list1.length === 0 && list2.length === 0) {
       return
     }
-    const [topExtensions, enableExtensions, disableExtensions] = await sortShowItems(
-      options,
-      list0,
-      list1,
-      list2
-    )
 
-    setItems({ top: topExtensions, enabled: enableExtensions, disabled: disableExtensions })
+    const [enableExtensions, disableExtensions] = await sortShowItems(options, list1, list2)
+
+    setItems({ top: list0, enabled: enableExtensions, disabled: disableExtensions })
   }
 
   useEffect(() => {
@@ -53,19 +49,18 @@ export const usePopupExtensions = (extensions, options, moved) => {
   return [items]
 }
 
-async function sortShowItems(options, list0, list1, list2) {
+async function sortShowItems(options, list1, list2) {
   // 先按照名称排序执行一次
-  const list0_pre = sortExtension(list0, { ignoreEnable: true })
   const list1_pre = sortExtension(list1)
   const list2_pre = sortExtension(list2)
 
   if (!options.setting.isSortByFrequency) {
-    return [list0_pre, list1_pre, list2_pre]
+    return [list1_pre, list2_pre]
   }
 
   // 如果有需要，再按照频率排序
   const refList = await manualEnableCounter.getOrder()
-  return [order(refList, list0_pre), order(refList, list1_pre), order(refList, list2_pre)]
+  return [order(refList, list1_pre), order(refList, list2_pre)]
 }
 
 async function buildShowItems(extensions, options) {
@@ -73,7 +68,7 @@ async function buildShowItems(extensions, options) {
   let list1 = list.filter((i) => i.enabled)
   let list2 = list.filter((i) => !i.enabled)
 
-  // 筛选置顶的扩展
+  // 筛选置顶的扩展, 返回的是已经排序的扩展 id 列表
   const topExtensions = await findTopExtensions(options)
   let list0 = list.filter((i) => topExtensions.includes(i.id))
   list1 = list1.filter((i) => !topExtensions.includes(i.id))
@@ -86,17 +81,29 @@ async function buildShowItems(extensions, options) {
     list2 = list2.filter((i) => !hiddenExtensions.includes(i.id))
   }
 
-  return [list0, list1, list2]
+  return [order(topExtensions, list0), list1, list2]
 }
 
 /**
- * 找出那些应该置顶显示的扩展
+ * 找出那些应该置顶显示的扩展；返回结果已经进行了排序
  */
 export async function findTopExtensions(options) {
   const byRule = await findTopExtensionsByRule(options)
-  const byTop = await findTopExtensionsByRecentlyUpdate(options)
+  const byRecently = await findTopExtensionsByRecentlyUpdate(options)
 
-  return Array.from(new Set([...byRule, ...byTop]))
+  if (byRule.length === 0) {
+    return byRecently
+  }
+
+  // 先按名称排序
+  let byRuleOrdered = sortExtension(byRule, { ignoreEnable: true })
+  // 再按照频率排序
+  if (options.setting.isSortByFrequency) {
+    const refList = await manualEnableCounter.getOrder()
+    byRuleOrdered = order(refList, byRuleOrdered)
+  }
+
+  return Array.from(new Set([...byRuleOrdered, ...byRecently]))
 }
 
 // 最近更新的扩展
@@ -117,22 +124,40 @@ async function findTopExtensionsByRecentlyUpdate(options) {
     }
   }
 
+  const normalizeTime = (t) => {
+    if (!t) {
+      return 0
+    }
+    return t
+  }
+
   const now = new Date().getTime()
   const expired = now - days * 24 * 60 * 60 * 1000
-  return records
-    .filter((r) => {
-      const installDate = r.installDate ?? 0
-      const updateDate = r.updateDate ?? 0
+  const items = records.filter((r) => {
+    const installDate = normalizeTime(r.installDate)
+    const updateDate = normalizeTime(r.updateDate)
 
-      if (installDate > expired) {
-        return true
-      }
-      if (mode === "update" && updateDate > expired) {
-        return true
-      }
-      return false
-    })
-    .map((r) => r.id)
+    if (installDate > expired) {
+      return true
+    }
+    if (mode === "update" && updateDate > expired) {
+      return true
+    }
+    return false
+  })
+
+  const sortedItems = items.sort((a, b) => {
+    const ta = Math.max(normalizeTime(a.installDate), normalizeTime(a.updateDate))
+    const tb = Math.max(normalizeTime(b.installDate), normalizeTime(b.updateDate))
+
+    console.log(a.name, ta, b.name, tb)
+
+    return tb - ta
+  })
+
+  console.log(sortedItems)
+
+  return sortedItems.map((i) => i.id)
 }
 
 // 由规则配置的，置顶的扩展
