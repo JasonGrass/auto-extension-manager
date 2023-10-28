624549d0a3353a4150a87570f11453b756b0a624
JasonGrass <junjiegrass@qq.com>
2023-10-28 11:33:03 +0800

    ✨ feat(setting): 添加缩放比例设置项

--- src/pages/Options/settings/components/ViewOtherSetting.jsx
+++ src/pages/Options/settings/components/ViewOtherSetting.jsx
@@ -10,12 +10,16 @@ const ViewOtherSetting = memo(({ setting, onSettingChange }) => {
   const [isSortByFrequency, setIsSortByFrequency] = useState(false)
   // Popup 暗色模式
   const [darkMode, setDarkMode] = useState("system")
+  // Popup 缩放比例
+  const [zoomRatio, setZoomRatio] = useState(100)
 
   useEffect(() => {
     const sortByFrequency = setting.isSortByFrequency ?? false
     setIsSortByFrequency(sortByFrequency)
     const initDarkMode = setting.darkMode ?? "system"
     setDarkMode(initDarkMode)
+    const ratio = setting.zoomRatio ?? 100
+    setZoomRatio(ratio)
   }, [setting])
 
   return (
@@ -49,6 +53,20 @@ const ViewOtherSetting = memo(({ setting, onSettingChange }) => {
           <Radio value="dark">{getLang("setting_dark_mode_dark")}</Radio>
           <Radio value="system">{getLang("setting_dark_mode_system")}</Radio>
         </Radio.Group>
+      </div>
+
+      {/* 缩放比例 */}
+      <div className="setting-item">
+        <span>{getLang("setting_popup_scale_title")}</span>
+        <Slider
+          style={{ width: 100, margin: "0 10px 0 0" }}
+          defaultValue={100}
+          value={zoomRatio}
+          onChange={(value) => onSettingChange(value, setZoomRatio, "zoomRatio")}
+          min={10}
+          max={100}
+          step={1}
+        />
       </div>
     </div>
   )
