315a8a540a214693ac34fe3bc4b727d4a824998a
JasonGrass <junjiegrass@qq.com>
2023-11-18 10:08:56 +0800

    ✨ feat(log): 控制自定义日志的输出

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,42 +1,70 @@
 class Logger {
-  showMoreLog = false
+  isDebugMode = false
 
   init = () => {
     const development = "development"
     const production = "production"
     try {
+      // eslint-disable-next-line no-undef
       const env = RUNTIME_ENV
       console.log("ENV", env)
 
       if (env === "development") {
         console.log("开发环境下，开启调试日志输出")
-        this.showMoreLog = true
+        this.isDebugMode = true
       }
     } catch (err) {
       console.log("LOGGER INIT ENV ERROR", err)
     }
   }
 
+  getLevel() {
+    try {
+      // eslint-disable-next-line no-undef
+      const level = globalThis?.logLevel ?? window?.logLevel
+      if (!level) {
+        return 3
+      }
+      const num = parseInt(level)
+      if (num < 0 || num > 5) {
+        return 3
+      }
+      return num
+    } catch {
+      return 3
+    }
+  }
+
   trace(...args) {
-    if (!this.showMoreLog) {
+    if (this.isDebugMode) {
+      console.trace(...args)
       return
     }
-    console.info(...args)
+    if (this.getLevel() === 1) {
+      console.trace(...args)
+    }
   }
 
   debug(...args) {
-    if (!this.showMoreLog) {
+    if (this.isDebugMode) {
+      console.debug(...args)
       return
     }
-    console.debug(...args)
+    if (this.getLevel() <= 2) {
+      console.debug(...args)
+    }
   }
 
   info(...args) {
-    console.info(...args)
+    if (this.getLevel() <= 3) {
+      console.info(...args)
+    }
   }
 
   warn(...args) {
-    console.warn(...args)
+    if (this.getLevel() <= 4) {
+      console.warn(...args)
+    }
   }
 
   error(...args) {
