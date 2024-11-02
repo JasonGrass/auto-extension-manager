f966324a2493d9558404aff1d4414b0f21d872e0
JasonGrass <junjiegrass@qq.com>
2024-11-02 12:03:58 +0800

    🐞 fix(规则执行): 修复后台静默打开标签页，规则不触发和页面刷新问题 #137

--- src/pages/Background/event/tabChangeEvent.js
+++ src/pages/Background/event/tabChangeEvent.js
@@ -1,6 +1,7 @@
 chrome.tabs.onUpdated.addListener(onTabUpdated)
 chrome.tabs.onActivated.addListener(onTabActivated)
 chrome.tabs.onRemoved.addListener(onTabRemoved)
+chrome.tabs.onCreated.addListener(onTabCreated)
 chrome.windows.onRemoved.addListener(onWindowRemoved)
 
 let _currentTabUpdatedCallback
@@ -21,6 +22,37 @@ function onTabRemoved(tabId, removeInfo) {
   if (_lastTabInfo?.id !== tabId) {
     _tabClosedCallback?.(tabId, removeInfo)
   }
+}
+
+async function onTabCreated(tab) {
+  if (tab.active) {
+    // 这里只处理后台静默打开的标签页
+    return
+  }
+
+  let waitingTime = 0
+  // 等待 tab 加载完成，最多等待 9 秒
+  while (tab.status === "loading") {
+    if (waitingTime > 9000) {
+      return
+    }
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, 200)
+    })
+    tab = await chrome.tabs.get(tab.id)
+    waitingTime += 200
+  }
+
+  const tabInfo = {
+    url: tab.url,
+    title: tab.title,
+    windowId: tab.windowId,
+    id: tab.id
+  }
+  _lastTabInfo = tabInfo
+  _currentTabUpdatedCallback?.(tabInfo)
 }
 
 function onWindowRemoved(windowId) {
