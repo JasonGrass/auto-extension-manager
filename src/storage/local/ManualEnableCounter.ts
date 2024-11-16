33194571eb1f1285e52bd52fc0b7930b2bb2d715
JasonGrass <junjiegrass@qq.com>
2024-11-16 11:48:26 +0800

    ✨ feat(Popup): 添加置顶显示最近启用扩展的功能 #169

--- src/storage/local/ManualEnableCounter.ts
+++ src/storage/local/ManualEnableCounter.ts
@@ -1,6 +1,8 @@
 import localforage from "localforage"
 
+// 最多记录的条数
 const MAX_COUNT = 110
+// 如果超过最多记录的条数，则删除最后面的 N 条
 const DELETE_COUNT_ONCE = 10
 
 export class ManualEnableCounter {
@@ -60,5 +62,47 @@ export class ManualEnableCounter {
 
     const result = Array.from(map.entries()).sort((a, b) => b[1] - a[1])
     return result.map(([key]) => key)
+  }
+
+  /**
+   * 获取最近手动启用的扩展的 ID 列表，按照时间排序，最新的在最前
+   */
+  async getRecentlyEnableIds() {
+    let keys = await this.forage.keys()
+
+    if (keys.length === 0) {
+      return []
+    }
+
+    const items = await Promise.all(
+      keys.map(async (key) => {
+        const id = await this.forage.getItem<string>(key)
+        return {
+          key,
+          id
+        }
+      })
+    )
+
+    // 第一步：使用 Map 记录每个 id 的最大 key
+    const maxKeyMap = new Map()
+
+    items.forEach((item) => {
+      const currentId = item.id
+      const currentKey = Number(item.key)
+
+      if (!maxKeyMap.has(currentId) || currentKey > maxKeyMap.get(currentId)) {
+        maxKeyMap.set(currentId, currentKey)
+      }
+    })
+
+    // 第二步：将结果转换为数组，并按 key 排序
+    const resultArray = Array.from(maxKeyMap, ([id, key]) => ({ id, key }))
+    resultArray.sort((a, b) => b.key - a.key)
+
+    // 第三步：提取出排序后的 id 列表
+    const sortedIdList = resultArray.map((item) => item.id)
+
+    return sortedIdList
   }
 }
