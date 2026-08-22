2c34a2494196d2d8dd066cf2e30f47f0ec288b6d
JasonGrass <junjiegrass@qq.com>
2026-08-22 20:34:40 +0800

    dev: 修改默认调试端口

--- utils/env.js
+++ utils/env.js
@@ -1,5 +1,5 @@
 // tiny wrapper with default env vars
 module.exports = {
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 5300,
-};
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 15301
+}
